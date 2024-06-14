e1e08918d8bf88fa8e46d74f06120ab630846fd0
agkayster <ejike.chiboka@gmail.com>
2024-06-14 12:43:04 +0100

    Implement input modal, submit button and cancel button for task

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -1,10 +1,13 @@
 import './Column.css';
-import React from 'react';
+import React, { useState } from 'react';
 import Tasks from './Tasks';
 import { useStore } from '../store';
 // import { shallow } from 'zustand/shallow';
 
 function Column({ state }) {
+	const [text, setText] = useState('');
+	const [open, setOpen] = useState(false);
+
 	// useStore should come from your store.js. filter our task.state that matches our state
 	const tasks = useStore((store) =>
 		store.tasks.filter((task) => task.state === state)
@@ -12,23 +15,46 @@ function Column({ state }) {
 
 	// import our addTask from store.js and pass it into our button element
 	const addTask = useStore((store) => store.addTask);
+
 	return (
 		<div className='column'>
 			<div className='titleWrapper'>
 				{/* receive state from App.js */}
 				<p>{state}</p>
-				<button
-					type=''
-					className=''
-					onClick={() => {
-						addTask('aaaaa' + state, state);
-					}}>
+				<button type='' className='' onClick={() => setOpen(true)}>
 					Add
 				</button>
 			</div>
 			{tasks.map((task) => (
 				<Tasks title={task.title} key={task.state} />
 			))}
+			{open && (
+				<div className='Modal'>
+					<div className='modalContent'>
+						<input
+							type='text'
+							className=''
+							value={text}
+							onChange={(e) => setText(e.target.value)}
+						/>
+						<button
+							onClick={() => {
+								addTask(text, state);
+								setText('');
+								setOpen(false);
+							}}>
+							Submit
+						</button>
+						<button
+							onClick={() => {
+								setText('');
+								setOpen(false);
+							}}>
+							Cancel
+						</button>
+					</div>
+				</div>
+			)}
 		</div>
 	);
 }
