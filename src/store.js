edaa95aa0f1d6ab6b866f7013b12c3d9e588ecf4
agkayster <ejike.chiboka@gmail.com>
2024-06-14 10:09:08 +0100

    initial implementation of zustand

---
+++ src/store.js
@@ -0,0 +1,13 @@
+import { create } from 'zustand';
+
+// contains everything we have in our store which is our tasks
+const store = (set) => ({
+	tasks: [
+		{
+			title: 'TestTask',
+			state: 'PLANNED',
+		},
+	],
+});
+// we just globalised our tasks
+export const useStore = create(store);
