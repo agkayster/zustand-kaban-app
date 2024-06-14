dbef0683df203be46135e49d4630289d8cde2079
agkayster <ejike.chiboka@gmail.com>
2024-06-14 10:30:08 +0100

    Implement dynamic presentation of tasks on the board

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -8,16 +8,6 @@ function Column({ state }) {
 	// useStore should come from your store.js
 	const tasks = useStore(
 		(store) => store.tasks.filter((task) => task.state === state)
-		// shallow
-		// (prev, next) => {
-		// 	const longest =
-		// 		prev.length > next.length ? prev.length : next.length;
-		// 	for (let i = 0; i < longest; i++) {
-		// 		if (!prev[i] || !next[i]) return false;
-		// 		if (prev[i] !== next[i]) return false;
-		// 	}
-		// 	return true;
-		// }
 	);
 	return (
     <div className='column'>
