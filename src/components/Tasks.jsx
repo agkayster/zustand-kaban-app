abbfa77fd75015aad7f4fcdde18bc6a58dff3c14
agkayster <ejike.chiboka@gmail.com>
2024-06-14 15:38:38 +0100

    implement delete task

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -2,17 +2,25 @@ import './Tasks.css';
 import React from 'react';
 import classNames from 'classnames';
 import { useStore } from '../store';
+import { FaRegTrashCan } from 'react-icons/fa6';
 
 const Tasks = ({ title }) => {
 	const task = useStore((store) =>
 		store.tasks.find((task) => task.title === title)
 	);
+
+	const deleteTask = useStore((store) => store.deleteTask);
 	return (
 		<div className='task'>
 			{/* title is received from Column.jsx */}
 			<div>{task.title}</div>
 			<div className='bottomWrapper'>
-				<div></div>
+				<div className='deleteBtn'>
+					<FaRegTrashCan
+						onClick={() => deleteTask(title)}
+						style={{ color: 'blue', fontSize: '1rem' }}
+					/>
+				</div>
 				<div className={classNames('status', task.state)}>
 					{task.state}
 				</div>
