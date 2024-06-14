4ddc97defc0c6d1258054865801ca758c6c34e34
agkayster <ejike.chiboka@gmail.com>
2024-06-14 16:14:33 +0100

    Implement draggable functionality for task

--- src/store.js
+++ src/store.js
@@ -8,13 +8,21 @@ const store = (set) => ({
 			state: 'DONE',
 		},
 	],
+
+	// keep/receive/persist what is being dragged into a column
+	draggedTask: null,
+
 	// use the set function because we want to manipulate our tasks
 	addTask: (title, state) =>
 		set((store) => ({ tasks: [...store.tasks, { title, state }] })),
+
 	deleteTask: (title) =>
 		set((store) => ({
 			tasks: store.tasks.filter((task) => task.title !== title),
 		})),
+
+	// give our dragged task a title and manipulate it
+	setDraggedTask: (title) => set({ draggedTask: title }),
 });
 // we just globalised our tasks
 export const useStore = create(store);
